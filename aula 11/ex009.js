7f29470aa034dd648861a700403082cd1602d918
Umbrella-crk <marcusbenicioro@gmail.com>
2026-04-29 04:31:59 -0300

    k

---
+++ aula 11/ex009.js
@@ -0,0 +1,9 @@
+var vel = 80
+console.log (`A velocidade do seu carro é (${vel} km/h)`)
+if (vel > 60) { // Condição simples
+    console.log (`Você ultrapassou a velocidade permitida. MULTADO!`)
+} else { // Condição composta é quando tem o else junto do if
+    console.log (`Dirija com segurança!`)
+}
+
+console.log (`Dirija sempre usando cinto de segurança!`)
