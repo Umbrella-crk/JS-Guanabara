147c73925ead5160176665a53a7af894f9c87063
Umbrella-crk <marcusbenicioro@gmail.com>
2026-04-30 08:48:19 -0300

    kkkkkkk

--- aula 12 ex/ex014/script.js
+++ aula 12 ex/ex014/script.js
@@ -3,7 +3,8 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas`
+var minutos = data.getMinutes()
+msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
 if (hora >= 0 && hora < 12){
     //Bom dia!
     img.src = 'dia.png'
