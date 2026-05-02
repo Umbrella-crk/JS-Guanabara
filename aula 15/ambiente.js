e838f2fe1165e67aed27fb6fb840fc9ed4717ac1
Umbrella-crk <marcusbenicioro@gmail.com>
2026-05-02 12:07:57 -0300

    Update ambiente.js

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,3 +1,12 @@
-let num = []
+let num = [6, 5, 9, 8, 1]
+/*
+for (let pos=0; pos < num.length; pos++) {
+    console.log(num[pos])
+}
 
-console.log('num')
+for (let pos in num) {
+    console.log(num[pos])
+}
+*/
+let pos = num.indexOf(1)
+console.log(pos)
